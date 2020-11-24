b874b4c7a248c55a781d518186b0747783e8499a
Stephen Brawner <brawner@gmail.com>
2020-11-24 12:34:47 -0800

    Handle potential divide by 0 (#267)
    * Handle potential divide by 0
    
    Signed-off-by: Stephen Brawner <brawner@gmail.com>
    
    * Use assert
    
    Signed-off-by: Stephen Brawner <brawner@gmail.com>

--- rmw_cyclonedds_cpp/src/Serialization.cpp
+++ rmw_cyclonedds_cpp/src/Serialization.cpp
@@ -357,7 +357,10 @@ protected:
   {
     align %= max_align;
 
-    if (align % get_cdr_alignof_primitive(v.type_kind()) != 0) {
+    // Value of 0 implies it is not a primitive, which should not happen and is checked elsewhere
+    const size_t cdr_alignof = get_cdr_alignof_primitive(v.type_kind());
+    assert(0 != cdr_alignof);
+    if (align % cdr_alignof != 0) {
       return false;
     }
     return v.sizeof_type() == get_cdr_size_of_primitive(v.type_kind());
