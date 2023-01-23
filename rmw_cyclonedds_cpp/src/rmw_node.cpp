3dcdcfa7573e1580db74940600a679fa99ce73f7
Sebastian Jakymiw <sjakymiw@irobot.com>
2023-01-23 14:55:30 -0500

    Fix return type
    Signed-off-by: Sebastian Jakymiw <sjakymiw@irobot.com>

--- rmw_cyclonedds_cpp/src/rmw_node.cpp
+++ rmw_cyclonedds_cpp/src/rmw_node.cpp
@@ -5559,5 +5559,5 @@ extern "C" bool rmw_feature_supported(rmw_feature_t feature)
 rmw_ret_t
 rmw_notify_participant_dynamic_network_interface(rmw_context_t * context)
 {
-  return RMW_RET_INCORRECT_RMW_IMPLEMENTATION;
+  return RMW_RET_UNSUPPORTED;
 }
