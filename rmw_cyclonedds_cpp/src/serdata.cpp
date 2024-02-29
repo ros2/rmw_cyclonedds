d8973e818db1546975478dc59699f78417b9051b
Tomoya Fujita <Tomoya.Fujita@sony.com>
2024-02-29 07:46:52 -0800

    compare string contents but string pointer addresses. (#481)
    Signed-off-by: Tomoya Fujita <Tomoya.Fujita@sony.com>

--- rmw_cyclonedds_cpp/src/serdata.cpp
+++ rmw_cyclonedds_cpp/src/serdata.cpp
@@ -53,12 +53,16 @@ using ResponseTypeSupport_cpp = rmw_cyclonedds_cpp::ResponseTypeSupport<
 
 static bool using_introspection_c_typesupport(const char * typesupport_identifier)
 {
-  return typesupport_identifier == rosidl_typesupport_introspection_c__identifier;
+  return strcmp(
+    typesupport_identifier,
+    rosidl_typesupport_introspection_c__identifier) == 0;
 }
 
 static bool using_introspection_cpp_typesupport(const char * typesupport_identifier)
 {
-  return typesupport_identifier == rosidl_typesupport_introspection_cpp::typesupport_identifier;
+  return strcmp(
+    typesupport_identifier,
+    rosidl_typesupport_introspection_cpp::typesupport_identifier) == 0;
 }
 
 void * create_message_type_support(
