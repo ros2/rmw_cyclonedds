f61e51bb0c58ceff73d1232a5a771db6d2154e30
Tony Najjar <tony.najjar@dexory.com>
2026-05-19 06:50:57 +0000

    fix uncrustify
    Signed-off-by: Tony Najjar <tony.najjar@dexory.com>

--- rmw_cyclonedds_cpp/src/rmw_node.cpp
+++ rmw_cyclonedds_cpp/src/rmw_node.cpp
@@ -570,9 +570,9 @@ extern "C" rmw_ret_t rmw_subscription_set_on_new_message_callback(
     // For KEEP_ALL history, depth is reported as 0 (since CycloneDDS internally
     // uses -1 for unlimited depth, which gets mapped to 0 in dds_qos_to_rmw_qos).
     // In that case, pass through the full unread_count instead of clipping to 0.
-    size_t events = (sub_qos.depth > 0)
-      ? std::min(data->unread_count, sub_qos.depth)
-      : data->unread_count;
+    size_t events = (sub_qos.depth > 0) ?
+      std::min(data->unread_count, sub_qos.depth) :
+      data->unread_count;
 
     callback(user_data, events);
     data->unread_count = 0;
