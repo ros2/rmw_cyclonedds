71b46452aa2cc1b4053e78e0088445f7a99e694b
Tomoya Fujita <fujita.tomoya@triorb.co.jp>
2026-08-07 16:56:05 +0900

    print rmem warning per-process, and adjust the output filter.
    Signed-off-by: Tomoya Fujita <fujita.tomoya@triorb.co.jp>

--- rmw_cyclonedds_cpp/cmake/get_rmw_cyclonedds_output_filter.cmake
+++ rmw_cyclonedds_cpp/cmake/get_rmw_cyclonedds_output_filter.cmake
@@ -27,5 +27,7 @@ macro(get_rmw_cyclonedds_output_filter output_patterns)
       "unused arguments: ${ARGN}")
   endif()
 
-  set(${output_patterns} ".*using network interface \\w+ \\(.*\\) selected arbitrarily from:.*")
+  string(CONCAT ${output_patterns}
+    ".*using network interface \\w+ \\(.*\\) selected arbitrarily from:.*\n"
+    ".*system rmem_max \\([0-9]+\\) is lower than the recommended minimum.*")
 endmacro()
